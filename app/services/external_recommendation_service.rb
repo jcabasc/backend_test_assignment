40239c7466e59c5f05c95f061e8791825488b44a
Jonathan Cabas <jcabas.ing@gmail.com>
2021-08-09 16:10:22 -0500

    Caches external api request
    - Rescues external call

--- app/services/external_recommendation_service.rb
+++ app/services/external_recommendation_service.rb
@@ -14,10 +14,14 @@ class ExternalRecommendationService
   end
 
   def call
-    response = JSON.parse(Net::HTTP.get(uri))
-    response.each_with_object(Hash.new(0)) do |element, hash|
-      hash[element['car_id']] = element['rank_score']
-    end.sort_by { |k, v| -v }.to_h
+    Rails.cache.fetch([uri.path, uri.query], expires: 6.hour) do
+      response = JSON.parse(Net::HTTP.get(uri))
+      response.each_with_object(Hash.new(0)) do |element, hash|
+        hash[element['car_id']] = element['rank_score']
+      end.sort_by { |k, v| -v }.to_h
+    end
+  rescue => e
+    {}
   end
 
   private
